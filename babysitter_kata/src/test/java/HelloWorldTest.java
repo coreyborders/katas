886ee6defdd63e0f2ce1b70e241500669cc73447
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2021-02-05 09:13:10 -0500

    initial commit

---
+++ babysitter_kata/src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloWorld(){
+        assertEquals(2, 1 + 1);
+    }
+
+}
