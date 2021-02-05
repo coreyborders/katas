de4766ec4bb2cd275f113592031669478b7a45e1
Austin.Kucera01 <austin.kucera01@cardinalhealth.com>
2021-02-05 09:47:09 -0500

    add pencil durability kata

---
+++ pencil_durability_kata/src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void HelloWorld() {
+        assertEquals(1, 2 - 1);
+    }
+
+}
