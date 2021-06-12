cc4ab326f98fbaf34f6cc72b251a04ca93cc8601
ManApart <r@austinkucera.com>
2021-06-12 12:08:23 -0400

    initial thoughts

--- babysitter_kata_kotlin/build.gradle.kts
+++ babysitter_kata_kotlin/build.gradle.kts
@@ -14,7 +14,11 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test-junit5"))
 
+    //This seems unnecessary
     testImplementation("org.mockito.kotlin:mockito-kotlin:2.2.11")
+
+    //This let's gradle build without the ide. This would be needed for ci/cd etc (I think the IDE is cheating for you + 'just working'
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
---
+++ babysitter_kata_kotlin/notes.md
@@ -0,0 +1,10 @@
+# Austin's Notes
+
+
+Thank you for making this a public repo! Much easier for me to review, and gives that sweet sweet github cred
+
+Mockito seems like a smell. Why is it needed?
+
+Pay attention to squigglies, they usually help you write better code.
+
+Tests pass in ide but not in the terminal, looks like you'd need to add a test platform. (See build.gradle for comments)
