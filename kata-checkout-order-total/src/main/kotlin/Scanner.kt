ef031bc12cfcfc6aecf0eac28cc564c1c2d659ea
Corey Borders <corey.borders@cardinalhealth.com>
2021-07-20 15:37:32 -0400

    Added code to be able to accept multiple scans of an item

--- kata-checkout-order-total/src/main/kotlin/Scanner.kt
+++ kata-checkout-order-total/src/main/kotlin/Scanner.kt
@@ -1,9 +1,19 @@
 class Scanner {
-    fun acceptItem(item: String): Double {
-        return when (item) {
+    fun acceptItem(item: String, units: Int): Double {
+        val unitPrice = when (item) {
             "soup" -> 2.99
             "gum" -> 0.99
             else -> 0.00
         }
+        return "%.2f".format(unitPrice * units).toDouble()
+    }
+
+    fun acceptItem(item: Int, units: Int): Double {
+        val unitPrice = when (item) {
+            1234 -> 2.99
+            2345 -> 0.99
+            else -> 0.00
+        }
+        return "%.2f".format(unitPrice * units).toDouble()
     }
 }
--- kata-checkout-order-total/src/test/kotlin/test.kt
+++ kata-checkout-order-total/src/test/kotlin/test.kt
@@ -6,15 +6,22 @@ class test {
 
     @Test
     fun scannedItemHasAPrice() {
-        val actual = scanner.acceptItem("soup")
-        val expected = 2.99
+        val actual = scanner.acceptItem("soup", 2)
+        val expected = 5.98
         assertEquals(actual, expected)
     }
 
     @Test
     fun scannedSoupHasADifferentPriceThanGum() {
-        val actual = scanner.acceptItem("gum")
-        val expected = 0.99
+        val actual = scanner.acceptItem("gum",3)
+        val expected = 2.97
+        assertEquals(actual, expected)
+    }
+
+    @Test
+    fun overloadedMethodInScannerWorksWithSKU() {
+        val actual = scanner.acceptItem(1234, 4)
+        val expected = 11.96
         assertEquals(actual, expected)
     }
 }
